2dfbd2be7d0fb8e63fe90c45f2e4fc5ee940c34d
slenthem <slenthem@1de3b9c8-f943-11de-ab06-dd2c9883db02>
2011-05-13 17:00:52 +0000

    setup only for 64bit os

---
+++ mirana/Configuration/Configuration/Installer.vb
@@ -0,0 +1,14 @@
+﻿Public Class Installer
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Hide()
+        SetupScreen.ShowDialog()
+        Me.Dispose()
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Me.Hide()
+        RemoveScreen.ShowDialog()
+        Me.Dispose()
+    End Sub
+End Class
---
+++ mirana/Configuration/Configuration/SetupScreen.vb
@@ -0,0 +1,25 @@
+﻿Public Class SetupScreen
+
+    Private CheckedLang As String
+
+    Private Sub RadioButton1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RadioButton1.CheckedChanged
+        CheckedLang = Me.RadioButton1.Text
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RadioButton2.CheckedChanged
+        CheckedLang = Me.RadioButton2.Text
+    End Sub
+
+    Private SetupRegistry As SetupRegistry
+    Private Message As String
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        SetupRegistry = New SetupRegistry
+        SetupRegistry.StartSetup(CheckedLang, Message)
+        Me.TextBox1.Text = Message
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Me.Dispose()
+    End Sub
+End Class
