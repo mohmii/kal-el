8c0734406a75d7c9e2f0cb7f1a8d7963570370f4
alfiezairul <alfiezairul@1de3b9c8-f943-11de-ab06-dd2c9883db02>
2011-10-10 04:42:21 +0000

    Tambahan kapabilitas keep - delete untuk add to opposite surface

---
+++ kal-el/AcadFR/AddToOppositeMsg.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Windows.Forms
+
+Public Class AddToOppositeMsg
+    Public KeepStat As New Boolean
+    Private Sub Keep_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Keep_Button.Click
+        KeepStat = True
+        If adskClass.myPalette.Label16.Text <> "0" Then
+            adskClass.myPalette.StartAddToOppositeSurface(adskClass.myPalette.IdentifiedFeature)
+        End If
+
+        If adskClass.myPalette.Label17.Text <> "0" Then
+            adskClass.myPalette.StartAddToOppositeSurface(adskClass.myPalette.UnidentifiedFeature)
+        End If
+
+        Me.DialogResult = System.Windows.Forms.DialogResult.Yes
+        Me.Close()
+    End Sub
+
+    Private Sub Delete_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Delete_Button.Click
+        KeepStat = False
+        If adskClass.myPalette.Label16.Text <> "0" Then
+            adskClass.myPalette.StartAddToOppositeSurface(adskClass.myPalette.IdentifiedFeature)
+            adskClass.myPalette.StartDeleting(adskClass.myPalette.IdentifiedFeature)
+        End If
+
+        If adskClass.myPalette.Label17.Text <> "0" Then
+            adskClass.myPalette.StartAddToOppositeSurface(adskClass.myPalette.UnidentifiedFeature)
+            adskClass.myPalette.StartDeleting(adskClass.myPalette.UnidentifiedFeature)
+        End If
+
+        Me.DialogResult = System.Windows.Forms.DialogResult.No
+        Me.Close()
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Close()
+    End Sub
+
+End Class
